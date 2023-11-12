40a198d0d291fa268976385565c7d629c005fe2e
anahi-luna <rebeluna263@gmail.com>
2023-11-12 19:32:02 -0300

    se creo el archivo productlist para almacenado de datos

---
+++ js/productList.js
@@ -0,0 +1,110 @@
+const products= [
+    {
+        id:1,
+        name:"Bife angosto",
+        character:"con papas y salsa",
+        price:2000,
+        images:"images/carne-menu.jpg",
+        count:1,
+        subTotal:2000
+    },
+    {
+        id:2,
+        name:"Tacos",
+        character:"con carne y vegetales",
+        price:2000,
+        images:"images/tacos-menu.jpg",
+        count:1,
+        subTotal:2000
+    },
+    {
+        id:3,
+        name:"Pizza",
+        character:"con peperoni y aceituna",
+        price:2000,
+        images:"images/pizza-menu.jpg",
+        count:1,
+        subTotal:2000
+    },
+    {
+        id:4,
+        name:"Hamburguesa",
+        character:"con papas y salsa",
+        price:2000,
+        images:"images/burger-menu.jpg",
+        count:1,
+        subTotal:2000
+    },
+    {
+        id:5,
+        name:"Bife angosto",
+        character:"con papas y salsa",
+        price:2000,
+        images:"images/carne-menu.jpg",
+        count:1,
+        subTotal:2000
+    },
+    {
+        id:6,
+        name:"Tacos",
+        character:"con carne y vegetales",
+        price:2000,
+        images:"images/tacos-menu.jpg",
+        count:1,
+        subTotal:2000
+    },
+    {
+        id:7,
+        name:"Pizza",
+        character:"con peperoni y aceituna",
+        price:2000,
+        images:"images/pizza-menu.jpg",
+        count:1,
+        subTotal:2000
+    },
+    {
+        id:8,
+        name:"Hamburguesa",
+        character:"con papas y salsa",
+        price:2000,
+        images:"images/burger-menu.jpg",
+        count:1,
+        subTotal:2000
+    },
+    {
+        id:9,
+        name:"Bife angosto",
+        character:"con papas y salsa",
+        price:2000,
+        images:"images/carne-menu.jpg",
+        count:1,
+        subTotal:2000
+    },
+    {
+        id:10,
+        name:"Tacos",
+        character:"con carne y vegetales",
+        price:2000,
+        images:"images/tacos-menu.jpg",
+        count:1,
+        subTotal:2000
+    },
+    {
+        id:11,
+        name:"Pizza",
+        character:"con peperoni y aceituna",
+        price:2000,
+        images:"images/pizza-menu.jpg",
+        count:1,
+        subTotal:2000
+    },
+    {
+        id:12,
+        name:"Hamburguesa",
+        character:"con papas y salsa",
+        price:2000,
+        images:"images/burger-menu.jpg",
+        count:1,
+        subTotal:2000
+    }
+]
